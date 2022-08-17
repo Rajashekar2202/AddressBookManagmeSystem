30d5fe46f54e328812d718ac0dd38b7d5bac3264
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-17 17:01:27 +0530

    added Creation of Contact Program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AddressBook
+{
+    class Program
+    {
+        public static List<Contact> contactList = new List<Contact>();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBookManagmentSystem");
+
+            Contact contact1 = new Contact();
+            Console.WriteLine("Enter first name: ");
+            contact1.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name: ");
+            contact1.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contact1.address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contact1.city = Console.ReadLine();
+
+            Console.WriteLine("Enter state: ");
+            contact1.state = Console.ReadLine();
+
+            Console.WriteLine("Enter phone: ");
+            contact1.phone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter email: ");
+            contact1.email = Console.ReadLine();
+
+            Console.WriteLine("Enter zipcode: ");
+            contact1.zipcode = Convert.ToInt32(Console.ReadLine());
+
+
+            contactList.Add(contact1);
+        }
+    }
+
+
+}
